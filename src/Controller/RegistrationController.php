370effcc8ad205c10f5da93fa975679537c9ec9d
dhaker bouzid <dhaker.bouzid300@gmail.com>
2026-05-10 10:45:20 +0100

    dhaker-creation des pages login et register

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager
+    ): Response {
+        // Redirect already-authenticated users
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hash and set password
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            // Assign the chosen role
+            $selectedRole = $form->get('role')->getData();
+            $user->setRoles([$selectedRole]);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Account created successfully! Please sign in.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Username',
+                'attr' => [
+                    'autocomplete' => 'username',
+                    'placeholder' => 'Choose a username',
+                ],
+                'constraints' => [
+                    new NotBlank(message: 'Please enter a username'),
+                    new Length(
+                        min: 3,
+                        minMessage: 'Username must be at least {{ limit }} characters',
+                        max: 180,
+                    ),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['placeholder' => 'your@email.com'],
+            ])
+            ->add('role', ChoiceType::class, [
+                'label'    => 'I am a',
+                'mapped'   => false,
+                'expanded' => true,   // renders as radio buttons
+                'multiple' => false,
+                'choices'  => [
+                    '🎓  Student (Étudiant)'       => User::ROLE_ETUDIANT,
+                    '👑  Club President (Président)' => User::ROLE_PRESIDENT,
+                ],
+                'constraints' => [
+                    new NotBlank(message: 'Please select a role'),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr'   => [
+                    'autocomplete' => 'new-password',
+                    'placeholder'  => 'At least 6 characters',
+                ],
+                'constraints' => [
+                    new NotBlank(message: 'Please enter a password'),
+                    new Length(
+                        min: 6,
+                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        max: 4096,
+                    ),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped'      => false,
+                'constraints' => [
+                    new IsTrue(message: 'You should agree to our terms.'),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
